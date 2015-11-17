d71f2689733857885953157f118f274e511d5ccd
liptomek <lipka.tomek@gmail.com>
2015-11-17 22:52:10 +0100

    * polish messages payload * rewrite archive messages delivery

--- src/main/java/io/chlipchat/Server.java
+++ src/main/java/io/chlipchat/Server.java
@@ -1,78 +1,52 @@
 package io.chlipchat;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.impl.StringEscapeUtils;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
-import io.vertx.ext.web.handler.sockjs.BridgeEventType;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
 import java.text.DateFormat;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 
 public class Server extends AbstractVerticle {
 
     public static final String ADDRESS_CHAT_TO_SERVER = "chat.to.server";
     public static final String ADDRESS_CHAT_TO_CLIENT = "chat.to.client";
-    public static final String ADDRESS_INIT = "init";
 
     @Override
     public void start() throws Exception {
+        JsonArray messages = new JsonArray();
+
         Router router = Router.router(vertx);
 
         BridgeOptions opts = new BridgeOptions()
                 .addInboundPermitted(new PermittedOptions().setAddress(ADDRESS_CHAT_TO_SERVER))
-                .addOutboundPermitted(new PermittedOptions().setAddress(ADDRESS_CHAT_TO_CLIENT))
-                .addOutboundPermitted(new PermittedOptions().setAddress(ADDRESS_INIT));
-
-
-        List<String> messages = Collections.synchronizedList(new ArrayList<>());
-
-        SockJSHandler ebHandler = SockJSHandler.create(vertx).bridge(opts, event -> {
-
-
-            if (event.type() == BridgeEventType.SOCKET_CREATED) {
-                messages.forEach(tmp -> {
-
-                    String tmp2 ="EMPTY";
-                    try {
-                        tmp2 = StringEscapeUtils.escapeJava(tmp);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    String value = "{ \n" +
-                            "  \"type\": \"rec\", \n" +
-                            "  \"address\": \"" + ADDRESS_INIT + "\", \n" +
-                            "  \"body\": \"" + tmp2 + "\"}";
-                    event.socket().write(Buffer.buffer(value));
-                });
-            }
-
-            event.complete(true);
-
-        });
+                .addOutboundPermitted(new PermittedOptions().setAddress(ADDRESS_CHAT_TO_CLIENT));
 
 
         router.route("/webjars/*").handler(webjarsHandler());
-        router.route("/eventbus/*").handler(ebHandler);
+        router.route("/eventbus/*").handler(SockJSHandler.create(vertx).bridge(opts));
         router.route().handler(staticHandler());
 
         vertx.createHttpServer().requestHandler(router::accept).listen(8080);
         EventBus eb = vertx.eventBus();
 
         eb.consumer(ADDRESS_CHAT_TO_SERVER).handler(message -> {
-            String timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()));
-            String value = "{ \"t\":\"" + timestamp + "\", \"p\":" + message.body() + "}";
-            eb.publish(ADDRESS_CHAT_TO_CLIENT, value);
-            messages.add(value);
+            JsonObject body = (JsonObject) message.body();
+            if("INIT".equals(body.getString("TYPE"))) {
+                message.reply(messages);
+            } else {
+                body.put("instant", Instant.now());
+                messages.add(body);
+                eb.publish(ADDRESS_CHAT_TO_CLIENT, body);
+
+            }
         });
     }
 
--- src/main/resources/webroot/chlipchat.js
+++ src/main/resources/webroot/chlipchat.js
@@ -1,31 +1,51 @@
-var login = window.prompt('Welcome to ChlipChat! \n Login?');
+var login = window.prompt('Welcome to ChlipChat!\nLogin?');
+
+const ADDRESS_TO_SERVER = "chat.to.server";
+const ADDRESS_TO_CLIENT = "chat.to.client";
+const LOCALE = 'en-GB';
 
 var eb = new EventBus("/eventbus/");
-var b;
+
+var chatWindow;
+var inputWindow;
+
+$(document).ready(function () {
+    chatWindow = $('#chat');
+    inputWindow = $('#input');
+});
+
 eb.onopen = function () {
-    eb.registerHandler("chat.to.client", function (err, msg) {
-        b = jQuery.parseJSON(msg.body);
-        $('#chat').append(b.t + ' <strong>' + escapeHTML(b.p.l) + '</strong>: ' + escapeHTML(b.p.m) + "\n");
-        $("#chat").scrollTop($("#chat")[0].scrollHeight);
-    });
-    eb.registerHandler("init", function (err, msg) {
-        b = jQuery.parseJSON(msg.body);
-        $('#chat').append(b.t + ' <strong>' + escapeHTML(b.p.l) + '</strong>: ' + escapeHTML(b.p.m) + "\n");
-        $("#chat").scrollTop($("#chat")[0].scrollHeight);
-    });
+    eb.registerHandler(ADDRESS_TO_CLIENT, receive);
+    eb.send(ADDRESS_TO_SERVER, {TYPE: 'INIT'}, receive);
 };
+
+function receive(err, msg) {
+    var messages = Array.isArray(msg.body) ? msg.body : [msg.body];
+    messages.forEach(function (message) {
+        chatWindow.append(timestamp(message.instant) + ' <strong>' + escape(message.login) + '</strong>: ' + escape(message.payload) + "\n");
+    });
+    chatWindow.scrollTop(chatWindow[0].scrollHeight);
+}
 
 function send(event) {
     if (event.keyCode == 13 || event.which == 13) {
-        var message = $('#input').val();
+        var message = inputWindow.val();
         if (message.length > 0) {
-            eb.publish("chat.to.server", {m: message, l: login});
-            $('#input').val("");
+            eb.publish(ADDRESS_TO_SERVER, {payload: message, login: login});
+            inputWindow.val('');
         }
     }
 }
 
-function escapeHTML(s) {
+function timestamp(instant) {
+    return new Date(instant).toLocaleString(locale());
+}
+
+function locale() {
+    return (navigator.languages && navigator.languages[0]) || navigator.language || LOCALE;
+}
+
+function escape(s) {
     return s.replace(/&/g, '&amp;')
         .replace(/"/g, '&quot;')
         .replace(/</g, '&lt;')
