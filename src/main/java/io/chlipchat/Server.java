a66f8b34322d5b1a6708e58b38cb08173303f5e9
liptomek <lipka.tomek@gmail.com>
2015-11-22 19:47:34 +0100

    new features
    * new design
    * users list
    * notifications

--- src/main/java/io/chlipchat/Server.java
+++ src/main/java/io/chlipchat/Server.java
@@ -1,23 +1,27 @@
 package io.chlipchat;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeEventType;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
-import java.text.DateFormat;
 import java.time.Instant;
-import java.util.Date;
 
 public class Server extends AbstractVerticle {
 
+    //TODO: need to have login
+
     public static final String ADDRESS_CHAT_TO_SERVER = "chat.to.server";
     public static final String ADDRESS_CHAT_TO_CLIENT = "chat.to.client";
+    public static final String ADDRESS_MAINTENANCE = "chat.MAINTENANCE";
+    public static final String USERS_MAP_NAME = "users";
 
     @Override
     public void start() throws Exception {
@@ -27,11 +31,11 @@ public class Server extends AbstractVerticle {
 
         BridgeOptions opts = new BridgeOptions()
                 .addInboundPermitted(new PermittedOptions().setAddress(ADDRESS_CHAT_TO_SERVER))
-                .addOutboundPermitted(new PermittedOptions().setAddress(ADDRESS_CHAT_TO_CLIENT));
-
+                .addOutboundPermitted(new PermittedOptions().setAddress(ADDRESS_CHAT_TO_CLIENT))
+                .addOutboundPermitted(new PermittedOptions().setAddress(ADDRESS_MAINTENANCE));
 
         router.route("/webjars/*").handler(webjarsHandler());
-        router.route("/eventbus/*").handler(SockJSHandler.create(vertx).bridge(opts));
+        router.route("/eventbus/*").handler(sockJSHandler(opts));
         router.route().handler(staticHandler());
 
         vertx.createHttpServer().requestHandler(router::accept).listen(8080);
@@ -39,14 +43,51 @@ public class Server extends AbstractVerticle {
 
         eb.consumer(ADDRESS_CHAT_TO_SERVER).handler(message -> {
             JsonObject body = (JsonObject) message.body();
-            if("INIT".equals(body.getString("TYPE"))) {
+            if("init".equals(body.getString("cmd"))) {
                 message.reply(messages);
-            } else {
+                eb.publish(ADDRESS_MAINTENANCE, new JsonObject()
+                        .put("cmd", "userJoined")
+                        .put("login", body.getString("login"))
+                        .put("handlerID", body.getString("handlerID")));
+                vertx.sharedData().getLocalMap(USERS_MAP_NAME)
+                        .put(body.getString("handlerID"), new JsonObject()
+                                .put("login", body.getString("login"))
+                                .put("handlerID",body.getString("handlerID")));
+            }
+            else if("user-list".equals(body.getString("cmd"))) {
+                JsonArray userList = new JsonArray();
+                vertx.sharedData().getLocalMap(USERS_MAP_NAME).values()
+                        .stream()
+                        .forEach(user -> userList.add(user));
+                message.reply(userList);
+            }
+            else {
                 body.put("instant", Instant.now());
                 messages.add(body);
                 eb.publish(ADDRESS_CHAT_TO_CLIENT, body);
-
             }
+        });
+    }
+
+    private SockJSHandler sockJSHandler(BridgeOptions opts) {
+        return SockJSHandler.create(vertx).bridge(opts, event -> {
+            if(event.type() == BridgeEventType.SOCKET_CREATED) {
+                event.socket().write(Buffer.buffer(new JsonObject()
+                        .put("type", "rec")
+                        .put("body", new JsonObject()
+                                .put("handlerID", event.socket().writeHandlerID())
+                                .put("cmd", "handlerID")
+                        )
+                        .put("address", ADDRESS_MAINTENANCE).encode()));
+            }
+            if(event.type() == BridgeEventType.SOCKET_CLOSED) {
+                vertx.eventBus().publish(ADDRESS_MAINTENANCE, new JsonObject()
+                        .put("cmd", "userLeft")
+                        .put("login", ((JsonObject) vertx.sharedData().getLocalMap(USERS_MAP_NAME).get(event.socket().writeHandlerID())).getString("login"))
+                        .put("handlerID", event.socket().writeHandlerID()));
+                vertx.sharedData().getLocalMap(USERS_MAP_NAME).remove(event.socket().writeHandlerID());
+            }
+            event.complete(true);
         });
     }
 
--- src/main/resources/webroot/chlipchat.js
+++ src/main/resources/webroot/chlipchat.js
@@ -1,7 +1,9 @@
 var login = window.prompt('Welcome to ChlipChat!\nLogin?');
+var handlerID;
 
 const ADDRESS_TO_SERVER = "chat.to.server";
 const ADDRESS_TO_CLIENT = "chat.to.client";
+const ADDRESS_MAINTENANCE = "chat.MAINTENANCE";
 const LOCALE = 'en-GB';
 
 var eb = new EventBus("/eventbus/");
@@ -9,29 +11,100 @@ var eb = new EventBus("/eventbus/");
 var chatWindow;
 var inputWindow;
 
+var Notification = window.Notification || window.mozNotification || window.webkitNotification;
+Notification.requestPermission(function (permission) {});
+
 $(document).ready(function () {
     chatWindow = $('#chat');
     inputWindow = $('#input');
+    inputWindow.focus();
+});
+
+var window_focus;
+
+$(window).focus(function() {
+    window_focus = true;
+}).blur(function() {
+    window_focus = false;
+});
+
+
+//TODO: move ADDRESS_MAINTENANCE receive handling to receive
+//TODO: notifications by mention, all, none
+
+var data = Bind({
+    users: []
+}, {
+    users: {
+        dom: '#user-list',
+        transform: function (value) {
+            return '<li>' + this.safe(value.login) + '</li>';
+        }
+    }
 });
 
 eb.onopen = function () {
     eb.registerHandler(ADDRESS_TO_CLIENT, receive);
-    eb.send(ADDRESS_TO_SERVER, {TYPE: 'INIT'}, receive);
+    eb.registerHandler(ADDRESS_TO_CLIENT, notify);
+    eb.registerHandler(ADDRESS_MAINTENANCE, function (err, msg) {
+        if(msg.body.cmd === 'handlerID') {
+            handlerID = msg.body.handlerID;
+            eb.send(ADDRESS_TO_SERVER, {cmd: 'user-list', handlerID: msg.body.handlerID, login: login}, function (err, msg) {
+                data.users = msg.body;
+            });
+            eb.send(ADDRESS_TO_SERVER, {cmd: 'init', handlerID: msg.body.handlerID, login: login}, receive);
+        }
+        if(msg.body.cmd === 'userJoined') {
+            data.users.push({ login: msg.body.login, handlerID: msg.body.handlerID });
+            chatWindow.append('<li><p>User <strong>' + escape(msg.body.login) + '</strong> joined</p></li>');
+            chatWindow.scrollTop(chatWindow[0].scrollHeight);
+        }
+        if(msg.body.cmd === 'userLeft') {
+            data.users = data.users.filter(function (e) {
+                return e.handlerID != msg.body.handlerID;
+            });
+            chatWindow.append('<li><p>User <strong>' + escape(msg.body.login) + '</strong> left</p></li>');
+            chatWindow.scrollTop(chatWindow[0].scrollHeight);
+        }
+    });
 };
+
+eb.onclose = function () {
+    alert('Connection to server closed, please reload');
+};
+
+function notify(err, msg) {
+    if(window_focus) return;
+    var messages = Array.isArray(msg.body) ? msg.body : [msg.body];
+    messages.forEach(function (message) {
+        if(message.handlerID != handlerID) {
+            var notification = new Notification(
+                escape(message.login) + ' ' + timestamp(message.instant)
+                , {
+                    body: escape(message.payload),
+                    icon: '/logo_small.png'
+                });
+            setTimeout(function(){
+                notification.close()
+            }, 5000);
+        }
+    });
+}
 
 function receive(err, msg) {
     var messages = Array.isArray(msg.body) ? msg.body : [msg.body];
     messages.forEach(function (message) {
-        chatWindow.append(timestamp(message.instant) + ' <strong>' + escape(message.login) + '</strong>: ' + escape(message.payload) + "\n");
+        chatWindow.append('<li><div class="message-data"><p>' + escape(message.login) + '</p> <p>' + timestamp(message.instant) + '</p></div><div class="message">' + escape(message.payload) + '</div></li>');
     });
     chatWindow.scrollTop(chatWindow[0].scrollHeight);
 }
 
 function send(event) {
+    //TODO: allow shift+enter line break console.log(!event.shiftKey && event.keyCode == 13 ? 'SEND' : 'NOT')
     if (event.keyCode == 13 || event.which == 13) {
-        var message = inputWindow.val();
+        var message = inputWindow.val().replace(/^\s+|\s+$/g, "");
         if (message.length > 0) {
-            eb.publish(ADDRESS_TO_SERVER, {payload: message, login: login});
+            eb.publish(ADDRESS_TO_SERVER, {payload: message, login: login, handlerID: handlerID});
             inputWindow.val('');
         }
     }
